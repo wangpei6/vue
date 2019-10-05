a586e61ce06eeac90712a1b757ede8c8fa09017f
wangpei6 <1481154160@qq.com>
2019-10-05 14:03:28 +0800

    第一天打卡

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 //入口文件
 import Vue from 'vue'
 import app from './app.vue'
-import {Header} from 'mint-ui'
+import {Header,Swipe, SwipeItem} from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+import './lib/mui/fonts/mui-icons-extra.ttf' 
+
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+//导入路由
+import router from './router.js'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 
 var vm = new Vue({
     el:'#app',
-    render:c=>c(app)
+    render:c=>c(app),
+    router//挂载路由对象
 })
--- src/router.js
+++ src/router.js
@@ -1,10 +1,19 @@
 
 import VueRouter from 'vue-router'
+import home from './lib/components/home.vue'
+import member from './lib/components/member.vue'
+import shopcar from './lib/components/shopcar.vue'
+import search from './lib/components/search.vue'
 
 var router = new VueRouter({
     routes:[
-        
-    ]
+        {path:'/',redirect:'/home'},
+        {path:'/home',component:home},
+        {path:'/member',component:member},
+        {path:'/shopcar',component:shopcar},
+        {path:'/search',component:search},
+    ],
+    linkActiveClass:'mui-active'
 })
 
 export default router
